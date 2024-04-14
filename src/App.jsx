c2c7e809baed088195ffc5098347638d136bcc4e
Olesia-Seleznova <106627666+Olesia-Seleznova@users.noreply.github.com>
2024-04-14 19:32:20 +0300

    add contacts

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,17 @@ import "./App.css";
 
 const getSavedContacts = () => {
   const savedContacts = localStorage.getItem("saved-contacts");
-  return savedContacts !== null ? JSON.parse(savedContacts) : [];
+  return savedContacts !== null
+    ? JSON.parse(savedContacts)
+    : [
+        { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+        { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+        { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+        { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+      ];
 };
 
 export default function App() {
-  // const initialContacts = [
-  //   { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-  //   { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-  //   { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-  //   { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  // ];
-
   const [contacts, setContacts] = useState(getSavedContacts());
   const [filter, setFilter] = useState("");
 
